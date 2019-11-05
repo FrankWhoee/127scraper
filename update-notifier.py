cb7e275476e62d75bfbf08c6a76dbdd7ec230d8c
Frank Hui <huifrank@live.com>
2019-11-04 22:24:58 -0800

    Add customization

--- update-notifier.py
+++ update-notifier.py
@@ -5,6 +5,9 @@ import subprocess as s
 import time
 from  scorescraper import get_score
 
+# Replace this with your username
+YOUR_USERNAME = 'bobby-tables'
+
 s.call(['notify-send','127 Scraper','Update notifier is working and will notify you.'])
 playsound('start.ogg')
 page = urllib.request.urlopen('https://www2.cs.sfu.ca/CourseCentral/127/common/results/')
@@ -22,7 +25,7 @@ while(1):
     newdate = soup.p.contents[0][27:51]
     if newdate != date:
         noti_str = ''
-        for score in get_score(['bobby-tables', 'Haxrox','vhhackspres']):
+        for score in get_score([YOUR_USERNAME]):
             noti_str += score + "\n"
         s.call(['notify-send', 'CMPT 127 Scoreboard Updated.', noti_str])
         playsound('updated.ogg')
