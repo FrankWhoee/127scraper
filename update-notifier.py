941704d8fb1988b61d9a42dcac7f1de965be6a4d
Frank Hui <huifrank@live.com>
2019-11-04 22:09:34 -0800

    Remove unneccesary elements

--- update-notifier.py
+++ update-notifier.py
@@ -5,7 +5,7 @@ import subprocess as s
 import time
 from  scorescraper import get_score
 
-s.call(['notify-send','127 Scraper','Starting scrape.'])
+s.call(['notify-send','127 Scraper','Update notifier is working and will notify you.'])
 playsound('start.ogg')
 page = urllib.request.urlopen('https://www2.cs.sfu.ca/CourseCentral/127/common/results/')
 
@@ -21,6 +21,9 @@ while(1):
     soup = BeautifulSoup(content, features="html.parser")
     newdate = soup.p.contents[0][27:51]
     if newdate != date:
-        s.call(['notify-send', 'CMPT 127 Scoreboard Updated.', get_score(['bobby-tables'])])
+        noti_str = ''
+        for score in get_score(['bobby-tables', 'Haxrox','vhhackspres']):
+            noti_str += score + "\n"
+        s.call(['notify-send', 'CMPT 127 Scoreboard Updated.', noti_str])
         playsound('updated.ogg')
-        date= newdate
+        date = newdate
